91d99b45c50c0be5401665da053a81a166ab5d9f
Hamxay <hamzach1288@gmail.com>
2023-05-11 19:13:43 +0500

    Working

--- android/app/src/main/java/com/reflect/MainActivity.java
+++ android/app/src/main/java/com/reflect/MainActivity.java
@@ -4,6 +4,11 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.content.Intent;
+import android.util.Log;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class MainActivity extends ReactActivity {
 
@@ -31,5 +36,41 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
         );
+  }
+  public boolean isOnNewIntent = false;
+ @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    isOnNewIntent = true;
+    ForegroundEmitter();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    if(isOnNewIntent == true){}else {
+        ForegroundEmitter();
+    }
+  }
+
+  public  void  ForegroundEmitter(){
+    // this method is to send back data from java to javascript so one can easily
+    // know which button from notification or the notification button is clicked
+    String  main = getIntent().getStringExtra("mainOnPress");
+    String  btn = getIntent().getStringExtra("buttonOnPress");
+    WritableMap  map = Arguments.createMap();
+    if (main != null) {
+        map.putString("main", main);
+    }
+    if (btn != null) {
+        map.putString("button", btn);
+    }
+    try {
+        getReactInstanceManager().getCurrentReactContext()
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit("notificationClickHandle", map);
+    } catch (Exception  e) {
+    Log.e("SuperLog", "Caught Exception: " + e.getMessage());
+    }
   }
 }
